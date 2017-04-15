5be64407a88b3ec7dd926c7d82e7e6b4b9ca3501
Lukaviy <jiukaviy@gmail.com>
2017-04-15 12:38:02 +1000

    Update sample program

--- main.cpp
+++ main.cpp
@@ -28,19 +28,23 @@ int main() {
 			} else {
 				printf("Disconnected\n");
 			}
-		});/*
-		communicator->OnOrientationReceive([](SimpleCommunicator_t::Orientation_t orientation)
+		});
+		communicator->OnOrientationReceive([](SimpleCommunicator_t::Orientation_t o)
 		{
-			printf("Orientation: %f, %f, %f, %f\n", orientation.q1, orientation.q2, orientation.q3, orientation.q4);
-		});*/
+			float angles[3];
+			angles[0] = atan2(2 * o.q2 * o.q3 - 2 * o.q1 * o.q4, 2 * o.q1 * o.q1 + 2 * o.q2 * o.q2 - 1); // psi
+			angles[1] = -asin(2 * o.q2 * o.q4 + 2 * o.q1 * o.q3); // theta
+			angles[2] = atan2(2 * o.q3 * o.q4 - 2 * o.q1 * o.q2, 2 * o.q1 * o.q1 + 2 * o.q4 * o.q4 - 1); // phi
+			//printf("Orientation: %f, %f, %f\r", angles[0], angles[1], angles[2]);
+		});
 		communicator->OnRawSensorDataReceive([](SimpleCommunicator_t::RawSensorData_t raw_sensor_data)
 		{
-			printf("\rRaw sensor data: %d, %d, %d, Depth: %f", raw_sensor_data.Ax, raw_sensor_data.Ay, raw_sensor_data.Az, raw_sensor_data.Depth);
+			printf("Raw sensor data: %d, %d, %d, Depth: %f\r", raw_sensor_data.Ax, raw_sensor_data.Ay, raw_sensor_data.Az, raw_sensor_data.Depth);
 		});
 		int len = 0;
 		communicator->OnCalibratedSensorDataReceive([&](SimpleCommunicator_t::CalibratedSensorData_t calibrated_sensor_data)
 		{
-			printf("\rCalibrated sensor data: %f, %f, %f  %f", calibrated_sensor_data.Mx, calibrated_sensor_data.My, calibrated_sensor_data.Mz, calibrated_sensor_data.Depth);
+			printf("Calibrated sensor data: %f, %f, %f  %f\r", calibrated_sensor_data.Ax, calibrated_sensor_data.Ay, calibrated_sensor_data.Az, calibrated_sensor_data.Depth);
 		});
 		communicator->OnI2CDevicesReceive([](SimpleCommunicator_t::I2CDevices_t devices)
 		{
@@ -65,10 +69,18 @@ int main() {
 			printf("MS5803: ");
 			printf(devices.MS5803 ? "connected\n" : "disconnected\n");
 		});
+
+		communicator->OnStop([](std::string error)
+		{
+			printf("Error: %s", error.c_str());
+		});
 		char c;
 		bool flashlight_state = false;
 		bool receive_raw_sensor_data = false;
 		bool receive_calibrated_sensor_data = false;
+
+
+		float thrust[6] = { 0, 0, 0, 0, 0, 0 };
 
 		communicator->Begin();
 		while (std::cin >> c) {
@@ -77,6 +89,12 @@ int main() {
 			case 'r': communicator->SetReceiveRawSensorData(receive_raw_sensor_data = !receive_raw_sensor_data); break;
 			case 'c': communicator->SetReceiveCalibratedSensorData(receive_calibrated_sensor_data = !receive_calibrated_sensor_data); break;
 			case 's': communicator->SetRescanI2CDevices(); break;
+			case 'm': {
+				int m_id;
+				std::cin >> m_id;
+				std::cin >> thrust[m_id];
+				communicator->SetMotorsState(thrust[0], thrust[1], thrust[2], thrust[3], thrust[4], thrust[5]);
+			} break;
 			}
 		}
 	} catch(ControllerException_t& e) {
