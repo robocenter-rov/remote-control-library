4d698ef6f96a1d38f2b17622278a4316a91e057f
Lukaviy <jiukaviy@gmail.com>
2017-05-05 16:36:58 +1000

    Update sample code

--- main.cpp
+++ main.cpp
@@ -8,7 +8,11 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 int main() {
-	ConnectionProvider_t* connection_provider = new UARTConnectionProvider_t("COM3", 115200, 200, 200);
+	std::string com_port_name;
+
+	std::cin >> com_port_name;
+
+	ConnectionProvider_t* connection_provider = new UARTConnectionProvider_t(com_port_name, 115200, 1024, 1024);
 	SimpleCommunicator_t* communicator = new SimpleCommunicator_t(connection_provider);
 
 	try {
@@ -37,14 +41,23 @@ int main() {
 			angles[2] = atan2(2 * o.q3 * o.q4 - 2 * o.q1 * o.q2, 2 * o.q1 * o.q1 + 2 * o.q4 * o.q4 - 1); // phi
 			//printf("Orientation: %f, %f, %f\r", angles[0], angles[1], angles[2]);
 		});
-		communicator->OnRawSensorDataReceive([](SimpleCommunicator_t::RawSensorData_t raw_sensor_data)
+		float gyro_x = 0, gyro_y = 0, gyro_z = 0;
+		int n = 0;
+		communicator->OnRawSensorDataReceive([&](SimpleCommunicator_t::RawSensorData_t raw_sensor_data)
 		{
-			printf("Raw sensor data: %d, %d, %d, Depth: %f\r", raw_sensor_data.Ax, raw_sensor_data.Ay, raw_sensor_data.Az, raw_sensor_data.Depth);
+			//printf("Raw sensor data: %d, %d, %d\r", raw_sensor_data.Gx, raw_sensor_data.Gy, raw_sensor_data.Gz);
+			gyro_x += raw_sensor_data.Gx;
+			gyro_y += raw_sensor_data.Gy;
+			gyro_z += raw_sensor_data.Gz;
+
+			n++;
+
+			printf("%f, %f, %f\r", gyro_x / n, gyro_y / n, gyro_z / n);
 		});
 		int len = 0;
 		communicator->OnCalibratedSensorDataReceive([&](SimpleCommunicator_t::CalibratedSensorData_t calibrated_sensor_data)
 		{
-			printf("Calibrated sensor data: %f, %f, %f  %f\r", calibrated_sensor_data.Ax, calibrated_sensor_data.Ay, calibrated_sensor_data.Az, calibrated_sensor_data.Depth);
+			printf("Calibrated sensor data: %f, %f, %f  %f\r", calibrated_sensor_data.Gx, calibrated_sensor_data.Gy, calibrated_sensor_data.Gz, calibrated_sensor_data.Depth);
 		});
 		communicator->OnI2CDevicesReceive([](SimpleCommunicator_t::I2CDevices_t devices)
 		{
@@ -69,6 +82,10 @@ int main() {
 			printf("MS5803: ");
 			printf(devices.MS5803 ? "connected\n" : "disconnected\n");
 		});
+		communicator->OnBluetoothMsgReceive([](std::string msg)
+		{
+			printf("Bluetooth message: %s\n", msg.c_str());
+		});
 
 		communicator->OnStop([](std::string error)
 		{
@@ -78,6 +95,7 @@ int main() {
 		bool flashlight_state = false;
 		bool receive_raw_sensor_data = false;
 		bool receive_calibrated_sensor_data = false;
+		bool read_bluetooth = false;
 
 
 		float thrust[6] = { 0, 0, 0, 0, 0, 0 };
@@ -107,13 +125,16 @@ int main() {
 				else if (c_id == 2) {
 					communicator->SetCamera2Pos(pos);
 				}
-			}
+			} break;
 			case 't': {
 				int d_id;
 				std::cin >> d_id;
-				std::cin >>	manipulator[d_id];
-				communicator->SetManipulatorState(manipulator[0], manipulator[1], 0, 0);
-			}
+				std::cin >> manipulator[d_id];
+				communicator->SetManipulatorState(manipulator[0], manipulator[1], manipulator[2], manipulator[3]);
+			} break;
+			case 'b': {
+				communicator->SetReadBluetoothState(read_bluetooth = !read_bluetooth);
+			} break;
 			}
 		}
 	} catch(ControllerException_t& e) {
