77e786ed1c0bfc5dbf81412c57547b73e4328bb3
Lukaviy <jiukaviy@gmail.com>
2017-06-12 18:56:33 +1000

    UDPConnectionProvider: Use bytearray instead of QByteArray

--- UDPConnectionProvider.cpp
+++ UDPConnectionProvider.cpp
@@ -1,10 +1,12 @@
 #include "UDPConnectionProvider.h"
 
-UDPConnectionProvider_t::UDPConnectionProvider_t(const QHostAddress &address, uint16_t port, size_t receive_buffer_size)
+UDPConnectionProvider_t::UDPConnectionProvider_t(const QHostAddress &address, uint16_t port, size_t receive_buffer_size, size_t send_buffer_size)
     : ConnectionProvider_t(receive_buffer_size),
     _address(address),
     _port(port),
-    _udp_socket(new QUdpSocket())
+    _udp_socket(new QUdpSocket()),
+    _send_buffer(new uint8_t[send_buffer_size]),
+    _send_buffer_size(send_buffer_size)
 {
     _received = 0;
     QObject::connect(
@@ -25,6 +27,7 @@ UDPConnectionProvider_t::UDPConnectionProvider_t(const QHostAddress &address, ui
 UDPConnectionProvider_t::~UDPConnectionProvider_t() {
     Stop();
     delete _udp_socket;
+    delete[] _send_buffer;
 }
 
 void UDPConnectionProvider_t::Begin() {
@@ -36,28 +39,37 @@ void UDPConnectionProvider_t::Stop() {
 }
 
 ConnectionProvider_t* UDPConnectionProvider_t::Write(const void *buffer, size_t size) {
-    _send_buffer.append((char *)buffer, size);
+    if (_send_buffer_size < _send_head + size) {
+        _send_head = 0;
+        throw SendBufferLimitExceeded_t();
+    }
+    memcpy(_send_buffer + _send_head, buffer, size);
     return this;
 }
 
 ConnectionProvider_t* UDPConnectionProvider_t::WriteUInt8(uint8_t c) {
-    _send_buffer.append(c);
+    if (++_send_head > _send_buffer_size) {
+        _send_head = 0;
+        throw SendBufferLimitExceeded_t();
+    }
+    _send_buffer[_send_head] = c;
     return this;
 }
 
 ConnectionProvider_t* UDPConnectionProvider_t::BeginPacket() {
-    _received = 0;
-    _send_buffer.clear();
+    _send_head = 0;
     return this;
 }
 
 ConnectionProvider_t* UDPConnectionProvider_t::EndPacket() {
-    _udp_socket->writeDatagram(_send_buffer, _address, _port);
+    _udp_socket->writeDatagram((char*)_send_buffer, _send_head, _address, _port);
     return this;
 }
 
 int UDPConnectionProvider_t::Receive() {
-    return _received;
+    size_t t = _received;
+    _received = 0;
+    return t;
 }
 
 bool UDPConnectionProvider_t::Connected() {
--- UDPConnectionProvider.h
+++ UDPConnectionProvider.h
@@ -13,10 +13,12 @@ private:
     QHostAddress _address;
     uint16_t _port;
     QUdpSocket* _udp_socket;
-    QByteArray _send_buffer;
-    int _received;
+    uint8_t* _send_buffer;
+    size_t _send_head;
+    size_t _send_buffer_size;
+    size_t _received;
 public:
-    explicit UDPConnectionProvider_t(const QHostAddress &address, uint16_t port, size_t receive_buffer_size);
+    explicit UDPConnectionProvider_t(const QHostAddress &address, uint16_t port, size_t receive_buffer_size, size_t send_buffer_size);
     ~UDPConnectionProvider_t() override;
     void Begin() override;
     void Stop() override;
